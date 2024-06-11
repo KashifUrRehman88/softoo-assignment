a7feba3079396573decaadff22a751101367993a
kashif <kaur@ciklum.com>
2024-06-11 13:58:16 +0500

    Committing first change i.e.

---
+++ coder-byte/src/test/java/com/coderbyte/challenge/CoderByteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderbyte.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderByteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
